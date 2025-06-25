a90cead0a19667abff4d28a374670c8ea321459c
nyshariyadh <nysha.riyadh@education.nsw.gov.au>
2025-06-25 11:12:20 +1000

    first edit maths menu program

---
+++ maths.py
@@ -0,0 +1,32 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def main():
+    print("what opeation you like to do?")
+    print("1. addition")
+    print("2. subtraction")
+
+    choice = input("please enter 1 or 2: ")
+
+    if choice in ['1', '2']:
+        try:
+            num1 = int(input("enter first number: "))
+            num2 = int(input("inter second number: "))
+
+            if choice == '1':
+                result = add(num1, num2)
+                print("the solution is:", result)
+            else:
+                result = subtract(num1, num2)
+                print("the solution is:", result)
+
+        except ValueError:
+            print("please enter valid numbers")
+    else:
+        print("invalid choice. please select 1 or 2")
+
+if __name__ == "__main__":
+    main()
